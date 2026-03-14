2fffa5d42d704d2d0766a1f6f796dfcafdd71414
Arun Kumar Thiagarajan <arunkumarthiags@gmail.com>
2026-03-13 21:12:33 -0700

    Delete finance-tracker directory

--- finance-tracker/backend/src/routes/categories.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as categoryController from '../controllers/categoryController';
-
-const router = Router();
-
-router.get('/', categoryController.getCategories);
-router.get('/:type', categoryController.getCategoriesByType);
-
-export default router;
